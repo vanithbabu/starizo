f62a562714161cb53afb1c57e947b7ced7f7100d
Vanith doodleblue <vanith@doodleblue.in>
2026-08-12 10:08:42 +0530

    chore: remove unused product assets and update database credentials in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'starizo_db' );
+define( 'DB_NAME', 'dev-starizo' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'dev-starizo_user' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'a(xgK~h?7}+y;}8N' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
